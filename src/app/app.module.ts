ea0853f13a33c6ab1f39c6675dc0602009b5ba78
matthieu debarge <debarge.matthieu@gmail.com>
2023-02-27 11:37:23 +0100

    6.11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from "./store/store.module";
 import { AppComponent } from './app.component';
+import { StoreComponent } from "./store/store.component";
+import { CheckoutComponent } from "./store/checkout.component";
+import { CartDetailComponent } from "./store/cartDetail.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -9,7 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    StoreModule
+    StoreModule,
+    RouterModule.forRoot([
+      { path: "store", component: StoreComponent },
+      { path: "cart", component: CartDetailComponent },
+      { path: "checkout", component: CheckoutComponent },
+      { path: "**", redirectTo: "/store" }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
