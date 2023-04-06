9fb7b12f8f3a41b5b442e680974e563526a05441
matthieu debarge <debarge.matthieu@gmail.com>
2023-04-06 20:35:27 +0200

    7.28

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,15 +6,29 @@ import { AuthComponent } from "./auth.component";
 import { AdminComponent } from "./admin.component";
 import { AuthGuard } from "./auth.guard";
 import { MaterialFeatures } from "./material.module";
+import { ProductTableComponent } from "./productTable.component";
+import { ProductEditorComponent } from "./productEditor.component";
+import { OrderTableComponent } from "./orderTable.component";
 let routing = RouterModule.forChild([
   { path: "auth", component: AuthComponent },
-  { path: "main", component: AdminComponent },
-  { path: "main", component: AdminComponent, canActivate: [AuthGuard] },
+  // { path: "main", component: AdminComponent },
+  // { path: "main", component: AdminComponent, canActivate: [AuthGuard] },
+  {
+    path: "main", component: AdminComponent, canActivate: [AuthGuard],
+    children: [
+      { path: "products/:mode/:id", component: ProductEditorComponent },
+      { path: "products/:mode", component: ProductEditorComponent },
+      { path: "products", component: ProductTableComponent },
+      { path: "orders", component: OrderTableComponent },
+      { path: "**", redirectTo: "products" }
+    ]
+  },
   { path: "**", redirectTo: "auth" }
 ]);
 @NgModule({
   imports: [CommonModule, FormsModule, routing, MaterialFeatures],
-  declarations: [AuthComponent, AdminComponent],
+  declarations: [AuthComponent, AdminComponent, ProductTableComponent,
+    ProductEditorComponent, OrderTableComponent],
   providers: [AuthGuard]
 })
 export class AdminModule { }
--- src/app/admin/material.module.ts
+++ src/app/admin/material.module.ts
@@ -4,8 +4,9 @@ import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from "@angular/material/button";
+import { MatTableModule } from "@angular/material/table";
 const features: any[] = [MatToolbarModule,MatSidenavModule,MatIconModule,
-  MatDividerModule, MatButtonModule];
+  MatDividerModule, MatButtonModule, MatTableModule];
 @NgModule({
   imports: [features],
   exports: [features]
