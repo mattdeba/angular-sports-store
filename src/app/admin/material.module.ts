959ff24df05f72ce3ba164ee1a6acbb09d0365f2
matthieu debarge <debarge.matthieu@gmail.com>
2023-04-06 20:22:44 +0200

    7.25

--- src/app/admin/material.module.ts
+++ src/app/admin/material.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from "@angular/core";
-const features: any[] = [];
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from "@angular/material/button";
+const features: any[] = [MatToolbarModule,MatSidenavModule,MatIconModule,
+  MatDividerModule, MatButtonModule];
 @NgModule({
   imports: [features],
   exports: [features]
---
+++ src/app/admin/productEditor.component.ts
@@ -0,0 +1,7 @@
+import {Component } from "@angular/core" ;
+@Component({
+  template : `<h3 style="padding-top : 10px">
+                Emplacement de l'éditeur de produit
+              </h3>`
+})
+export class ProductEditorComponent { }
