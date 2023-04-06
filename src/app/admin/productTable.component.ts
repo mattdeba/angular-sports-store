25eebfc218722621c06d678c1ade98dce275e1cc
matthieu debarge <debarge.matthieu@gmail.com>
2023-04-06 20:46:31 +0200

    7.31

--- src/app/admin/productTable.component.ts
+++ src/app/admin/productTable.component.ts
@@ -1,9 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, IterableDiffer, IterableDiffers } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
 @Component({
-  template: `
-        <h3 style="padding-top: 10px">
-            Product Table Placeholder
-        </h3>
-    `
+  templateUrl: "productTable.component.html"
 })
-export class ProductTableComponent {}
+export class ProductTableComponent {
+  colsAndRows: string[] = ['id', 'name', 'category', 'price', 'buttons'];
+  dataSource = new MatTableDataSource<Product>(this.repository.getProducts());
+  differ: IterableDiffer<Product>;
+  constructor(private repository: ProductRepository, differs: IterableDiffers) {
+    this.differ = differs.find(this.repository.getProducts()).create();
+  }
+  ngDoCheck() {
+    let changes = this.differ?.diff(this.repository.getProducts());
+    if (changes != null) {
+      this.dataSource.data = this.repository.getProducts();
+    }
+  }
+  deleteProduct(id: number) {
+    this.repository.deleteProduct(id);
+  }
+}
